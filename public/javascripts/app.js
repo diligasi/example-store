1416cd0bf4d470f0ff8c2a5e0bbbfd4beeebe059
Diogo de Lima <diligasi@gmail.com>
2017-06-12 17:12:01 -0300

    Add contact form validation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,23 +5,65 @@
 
     $(document).on('submit', 'form#contactForm', function(e) {
       e.preventDefault();
-      // var _url = 'http://192.168.0.111:3001/contacts';
-      var _url = 'https://dili-user-tracker.herokuapp.com/contacts';
-      $.ajax({
-          type: 'POST',
-          url: _url,
-          data: $(this).serialize(),
-          success: function(data) {
-            alert('Operação realizada com sucesso.');
-            window.location.reload();
-          },
-          error: function(el) {
-            console.log('***********************************');
-            console.log(JSON.stringify(el));
-            console.log('***********************************');
+      // var _url = 'http://192.168.0.111:3001/contacts',
+      var _url = 'https://dili-user-tracker.herokuapp.com/contacts',
+          valid = true,
+          emailValid = emailIsValid();
+
+      $(this).find(':input').each(function() {
+          if ($(this).val() === '') {
+              valid = false;
+              return valid;
           }
       });
+
+      if (valid && emailValid) {
+          $.ajax({
+              type: 'POST',
+              url: _url,
+              data: $(this).serialize(),
+              success: function(data) {
+                alert('Operação realizada com sucesso.');
+                window.location.reload();
+              },
+              error: function(el) {
+                console.log('***********************************');
+                console.log(JSON.stringify(el));
+                console.log('***********************************');
+              }
+          });
+      } else {
+          if ($('#contact-email').val() === '') {
+              alert('Por favor preencha todos os campos abaixo.');
+          }
+      }
     });
 
+    function emailIsValid() {
+        if ($('#contact-email').val() === '') {
+            return false;
+        }
+        $.ajax({
+            type: 'POST',
+            // url: 'http://192.168.0.111:3001/verify_email',
+            url: 'https://dili-user-tracker.herokuapp.com/verify_email',
+            data: { email: $('#contact-email').val() },
+            success(data) {
+                if (data.valid) {
+                    $('#send-contact').prop('disabled', false);
+                    return true;
+                } else {
+                    alert('E-mail já cadastrado.');
+                    $('#send-contact').prop('disabled',true);
+                    return false;
+                }
+            },
+            error(el) {
+                console.log('***********************************');
+                console.log(JSON.stringify(el));
+                console.log('***********************************');
+            }
+        });
+    }
   }); // end of document ready
 })(jQuery); // end of jQuery name space
